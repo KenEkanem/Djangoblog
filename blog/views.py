642422028491e1eb93901f380d920ff22407ffc7
KenEkanem <kennedyekanem@gmail.com>
2021-04-28 13:26:39 +0100

    List and detailed View

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,10 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView # new 
 from .models import Post
-
 
 class BlogListView(ListView): 
     model = Post 
     template_name = 'home.html'
 
-
 class BlogDetailView(DetailView): 
     model = Post 
     template_name = 'post_detail.html'
-
